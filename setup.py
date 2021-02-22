2ddedbcfd19a932434499348fadb51771ab2408b
Karol Krizka <kkrizka@gmail.com>
2021-02-22 15:51:10 -0800

    Add metadata to setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
+import pathlib
 import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name='httptime',
     version='0.1',
     description='Module for timing http queries inside a python program.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkrizka/httptime",
+    author="Karol Krizka",
+    author_email="kkrizka@gmail.com",
     packages=[
         'httptime'
         ]
